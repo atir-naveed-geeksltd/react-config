ac4ace88bbf48e2e0c9499257d346234a7dde7e2
Atir Naveed <atir.naveed@geeks.ltd.uk>
2022-01-05 23:57:02 +0000

    update webpack

--- packages/webpack-config/configs/webpack.common.js
+++ packages/webpack-config/configs/webpack.common.js
@@ -33,6 +33,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
